8c953b8ee77d92a05eb3936e507a0f4c913b3ec1
Alexander Shatravka <shatravgo2@gmail.com>
2020-04-28 23:51:50 +0300

    test global elements

---
+++ footer.php
@@ -0,0 +1,49 @@
+<footer id="footer">
+    <div class="container">
+        <div class="seo-breadcrumbs"><span typeof="v:Breadcrumb"><a href="https://cashyou.ua/ru/" rel="v:url"
+                                                                    property="v:title">cashyou.ua</a> › </span>
+            <span typeof="v:Breadcrumb"><a href="https://cashyou.ua/ru/contacts/" rel="v:url" property="v:title">❶❰КРЕДИТ❷❰ОНЛАЙН❸❰УКРАИНА</a></span>
+        </div>
+        <div class="logo"><a href="/"><img src="/images/logo-footer.png" alt="CASHYOU"></a></div>
+        <div class="footer-holder">
+            <div class="col"><strong class="conditions"><a href="/confidentiality.html">Умови користування</a></strong>
+            </div>
+            <div class="col">
+                <ul class="social-networks">
+                    <li class="facebook"><a href="https://www.facebook.com/Cashyou-371352639870774/"> <i
+                                class="icon-facebook"></i></a></li>
+                    <li class="twitter"><a href="https://twitter.com/CashYou_Ukraine"> <i class="icon-twitter"></i></a>
+                    </li>
+                    <li class="instagram"><a href="https://www.instagram.com/cashyou_official/"> <i
+                                class="icon-instagram"></i></i></i></a></li>
+                </ul>
+                <span class="phone"><a href="tel:0800212300">0800-21-23-00</a></span>
+            </div>
+        </div>
+        <a class="btn-top" href="#">Назад</a>
+    </div>
+    <ul class="tags">
+        <li><a href="/seo-articles/money-for-business.html">де взяти гроші на відкриття бізнесу</a></li>
+        <li><a href="/seo-articles/ways-to-economy.html">способи що дозволяють економити</a></li>
+        <li><a href="/seo-articles/how-rollover-loan.html">як пролонгувати кредит</a></li>
+        <li><a href="/seo-articles/banks-not-gives-loan.html">чому банки не дають кредит</a></li>
+        <li><a href="/seo-articles/smart-economy.html">як економити гроші правильно</a></li>
+        <li><a href="/seo-articles/credit-without-reject.html">кредит без відмови</a></li>
+        <li><a href="/seo-articles/credit-365.html">кредит-365</a></li>
+        <li><a href="/seo-articles/credit-bad-history.html">кредит з поганою історією</a></li>
+        <li><a href="/seo-articles/interest-rate.html">відсоткова ставка</a></li>
+        <li><a href="/seo-articles/loan-18.html">кредит з 18 років</a></li>
+        <li><a href="/seo-articles/credit-on-card.html">кредит на карту терміново</a></li>
+        <li><a href="/seo-articles/credit-unemployed.html">кредит безробітним</a></li>
+        <li><a href="/seo-articles/credit-without-income-statement.html">взяти кредит без справки про дохідність</a></li>
+        <li><a href="/seo-articles/credit-calculation.html">розрахунок кредита</a></li>
+    </ul>
+</footer>
+<div class="footer-icons-payment">
+    <img src="/images/visa-card.png" alt="">
+    <img src="/images/way4pay.png" alt="">
+    <img src="/images/comod.png" alt="">
+</div>
+</div>
+</body>
+</html>
